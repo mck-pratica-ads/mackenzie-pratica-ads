280f11e8a16f8f9e1541f0566c3eda8e1b60a52e
Lucas Torquato <lucastorquato@gmail.com>
2021-03-30 18:36:58 -0300

    Implement login request with error alert

---
+++ src/components/home.component.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import { Spinner } from 'react-bootstrap';
+
+export default class Home extends Component {
+
+    render() {
+        return (
+            <div>
+                <h3>Home</h3>
+
+                
+            </div>
+        );
+    }
+}
--- src/components/login.component.js
+++ src/components/login.component.js
@@ -1,32 +1,90 @@
 import React, { Component } from "react";
+import { Spinner, Alert } from 'react-bootstrap';
+import LoginService from "../services/LoginService";
 
 export default class Login extends Component {
+
+    constructor(props){
+        super(props);
+        this.state ={
+            username: undefined,
+            password: undefined,
+            isLoading: false,
+            hasError: false
+        }
+    }
+
+    submitLogin = (e) => {        
+        e.preventDefault();    
+        this.setState({isLoading: true,  hasError: false});
+
+        const { username, password } = this.state;
+        LoginService.loginUser({username: username, password: password})
+            .then(response => {
+                console.log("Success ========>", response);
+                if (response.data.data != null) {
+                    this.setState({isLoading: false});
+                this.props.history.push('/home');
+                } else {
+                    this.setState({isLoading: false, hasError: true});    
+                }                
+            })
+            .catch(error => {
+                console.log("Error ========>", error);
+                this.setState({isLoading: false, hasError: true});
+            });
+    }
+
     render() {
+        const { username, password, isLoading, hasError } = this.state;
+
         return (
-            <form>
-                <h3>Sign In</h3>
+            <form onSubmit={this.submitLogin}>
+                {hasError ? 
+                (<Alert key={'danger'} variant={'danger'}>
+                    Não foi possível fazer seu login. Por favor, verifique seu username e senha.
+                    </Alert>) : (<div></div>)}
+                <h3>Login</h3>
 
+                {isLoading ? (
+                    <div className="d-flex justify-content-center">
+                        <Spinner animation="border" />                        
+                    </div>
+                ) : (<div>
                 <div className="form-group">
-                    <label>Email address</label>
-                    <input type="email" className="form-control" placeholder="Enter email" />
+                    <label>Username</label>
+                    <input 
+                        type="username" 
+                        value={username} 
+                        className="form-control" 
+                        placeholder="Username" 
+                        onChange={(e) => {this.setState({username: e.target.value})}}
+                    />
                 </div>
 
                 <div className="form-group">
-                    <label>Password</label>
-                    <input type="password" className="form-control" placeholder="Enter password" />
+                    <label>Senha</label>
+                    <input 
+                        type="password" 
+                        value={password} 
+                        className="form-control" 
+                        placeholder="Password"
+                        onChange={(e) => {this.setState({password: e.target.value})}}
+                    />
                 </div>
 
-                <div className="form-group">
+                {/* <div className="form-group">
                     <div className="custom-control custom-checkbox">
                         <input type="checkbox" className="custom-control-input" id="customCheck1" />
                         <label className="custom-control-label" htmlFor="customCheck1">Remember me</label>
                     </div>
-                </div>
+                </div> */}
 
-                <button type="submit" className="btn btn-primary btn-block">Submit</button>
-                <p className="forgot-password text-right">
+                <button type="submit" className="btn btn-primary btn-block">Entrar</button>
+                {/* <p className="forgot-password text-right">
                     Forgot <a href="#">password?</a>
-                </p>
+                </p> */}
+                </div>)}
             </form>
         );
     }

c31fe5dddc10e7acc70021f299d2645e4df44aa8
Lucas Torquato <lucastorquato@gmail.com>
2021-05-02 18:55:14 -0300

    Improve welcome component

--- src/components/welcome.component.js
+++ src/components/welcome.component.js
@@ -5,22 +5,23 @@ export default class Welcome extends Component {
 
     render() {
         return (
-            <Container>
-                <Row className="justify-content-md-center">
-                    {/* <Col xs={12} sm={4} md={4}> */}
-                        <Image src="logo192.png" roundedCircle />
-                    {/* </Col> */}
-                </Row>
-                <Row className="justify-content-md-center">
-                    <Col md={0}>
-                        <a type="submit" className="btn btn-primary btn-block" href="/sign-in">Login</a>
-                    </Col>
-                    <Col md={0}>
-                        <a type="submit" className="btn btn-primary btn-block" href="/sign-up">Cadastro</a>
-                    </Col>
-                </Row>
-                
-            </Container>
+            <div className="auth-wrapper">
+                <div className="auth-inner">
+                    <Row className="justify-content-md-center">
+                        {/* <Col xs={12} sm={4} md={4}> */}
+                            <Image src="logo192.png" roundedCircle />
+                        {/* </Col> */}
+                    </Row>
+                    <Row className="justify-content-md-center">
+                        <Col md={0}>
+                            <a type="submit" className="btn btn-primary btn-block" href="/sign-in">Login</a>
+                        </Col>
+                        <Col md={0}>
+                            <a type="submit" className="btn btn-primary btn-block" href="/sign-up">Cadastro</a>
+                        </Col>
+                    </Row>
+                </div>
+            </div>
         );
     }
 }
